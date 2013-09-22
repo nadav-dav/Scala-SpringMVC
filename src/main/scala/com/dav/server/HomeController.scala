8bf815429ba149177722399ab9216f8ecf4456c3
Nadav L <nadavl@wix.com>
2013-09-22 11:20:25 +0300

    cleanup

--- src/main/scala/com/dav/server/HomeController.scala
+++ src/main/scala/com/dav/server/HomeController.scala
@@ -2,9 +2,6 @@ package com.dav.server
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{ResponseBody, RequestMapping}
-import org.springframework.web.servlet.DispatcherServlet
-import org.springframework.web.context.WebApplicationContext
-import org.springframework.web.context.support.XmlWebApplicationContext
 
 @Controller
 class HomeController {
